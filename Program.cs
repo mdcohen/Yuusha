6725681d1f6bb6e1dee65bc908707639c2b683df
mdcohen <mikeca26@gmail.com>
2019-09-21 09:20:54 +0200

    Added NVIDIA DLL load

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ namespace Yuusha
 {
     static class Program
     {
+        [System.Runtime.InteropServices.DllImport("nvapi64.dll", EntryPoint = "fake")]
+        private static extern int LoadNvApi64();
+
+        [System.Runtime.InteropServices.DllImport("nvapi.dll", EntryPoint = "fake")]
+        private static extern int LoadNvApi32();
+
         public static Client Client;
 
         /// <summary>
@@ -11,8 +17,22 @@ namespace Yuusha
         /// </summary>
         static void Main(string[] args)
         {
+            TryForceHighPerformanceGpu();
+
             Client = new Client();
             Client.Run();
+        }
+
+        static void TryForceHighPerformanceGpu()
+        {
+            try
+            {
+                if (Environment.Is64BitProcess)
+                    LoadNvApi64();
+                else
+                    LoadNvApi32();
+            }
+            catch { } // this is always triggered
         }
     }
 }
